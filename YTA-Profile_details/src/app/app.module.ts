95e54ffc553290053bab39b68f26a733d0ba3636
lkaveeshan <lalitharankaveeshan@gmail.com>
2022-11-13 15:06:29 +0530

    Commit

---
+++ YTA-Profile_details/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { HiringComponent } from './hiring/hiring.component';
+import { PeopleComponent } from './people/people.component';
+import { TeamsComponent } from './teams/teams.component';
+import { SublevelMenuComponent } from './sidenav/sublevel-menu.component';
+import { ListViewComponent } from './list-view/list-view.component';
+import { NavbarComponent } from './navbar/navbar.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    BodyComponent,
+    SidenavComponent,
+    EmployeesComponent,
+    HiringComponent,
+    PeopleComponent,
+    TeamsComponent,
+    SublevelMenuComponent,
+    ListViewComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
